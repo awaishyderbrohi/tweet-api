733318c4c3bac3d5371c2aec4a8bff189331ccea
awaishyder <awaisbrohi000@gmail.com>
2024-11-05 15:01:00 +0500

    first commit

---
+++ tweet-api/src/main/java/com/example/tweet_api/TweetApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tweet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetApiApplication.class, args);
+	}
+
+}
