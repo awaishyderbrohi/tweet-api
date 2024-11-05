a55d8304fb474ee76b30ca543047329195fd5a6c
awaishyder <awaisbrohi000@gmail.com>
2024-11-05 16:02:19 +0500

    TweetTest successful

--- tweet-api/src/test/java/com/example/tweet_api/entity/TweetTest.java
+++ tweet-api/src/test/java/com/example/tweet_api/entity/TweetTest.java
@@ -1,4 +1,10 @@
+package com.example.tweet_api.entity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class TweetTest {
-  
+
 }
